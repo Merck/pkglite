f2c1a831b083fede9dbe109ec5bcd966604e778f
Nan Xiao <nan.xiao1@merck.com>
2021-03-04 21:18:21 -0500

    ignore CRLF difference in snapshot tests

--- tests/testthat/test-independent_test_pack.R
+++ tests/testthat/test-independent_test_pack.R
@@ -13,7 +13,7 @@ save_txt <- function(code) {
 testthat::test_that("pack returns the text file correctly", {
   local_edition(3)
   x <- pack(fc1, fc2, output = txt, quiet = TRUE)
-  testthat::expect_snapshot_file(save_txt(x %>% readLines()), "pack.txt")
+  testthat::expect_snapshot_file(save_txt(x %>% readLines()), "pack.txt", binary = FALSE)
 })
 
 testthat::test_that("get_fc_pkg returns the package name correctly", {
@@ -23,8 +23,8 @@ testthat::test_that("get_fc_pkg returns the package name correctly", {
 testthat::test_that("fc_to_text returns the strings correctly", {
   local_edition(3)
   lst_fc <- list(fc1, fc2)
-  testthat::expect_snapshot_file(save_txt(fc_to_text(lst_fc[[1]]) %>% unlist()), "fc_to_text1.txt")
-  testthat::expect_snapshot_file(save_txt(fc_to_text(lst_fc[[2]]) %>% unlist()), "fc_to_text2.txt")
+  testthat::expect_snapshot_file(save_txt(fc_to_text(lst_fc[[1]]) %>% unlist()), "fc_to_text1.txt", binary = FALSE)
+  testthat::expect_snapshot_file(save_txt(fc_to_text(lst_fc[[2]]) %>% unlist()), "fc_to_text2.txt", binary = FALSE)
 })
 
 testthat::test_that("file_to_vec returns the strings correctly", {
@@ -40,18 +40,18 @@ testthat::test_that("file_to_vec returns the strings correctly", {
     file_to_vec(df[i, "path_abs"], format = df[i, "format"], pkg_name = pkg_name, path_rel = path_rel)
   }) %>% unlist()
 
-  testthat::expect_snapshot_file(save_txt(res), "file_to_vec.txt")
+  testthat::expect_snapshot_file(save_txt(res), "file_to_vec.txt", binary = FALSE)
 })
 
 testthat::test_that("read_file_text returns the strings correctly", {
   local_edition(3)
   fc <- list(fc1, fc2)
   df <- fc[[1]][["df"]]
-  testthat::expect_snapshot_file(save_txt(read_file_text(df[1, "path_abs"])), "read_file_text1.txt")
-  testthat::expect_snapshot_file(save_txt(read_file_text(df[2, "path_abs"])), "read_file_text2.txt")
-  testthat::expect_snapshot_file(save_txt(read_file_text(df[3, "path_abs"])), "read_file_text3.txt")
-  testthat::expect_snapshot_file(save_txt(read_file_text(df[4, "path_abs"])), "read_file_text4.txt")
-  testthat::expect_snapshot_file(save_txt(read_file_text(df[5, "path_abs"])), "read_file_text5.txt")
+  testthat::expect_snapshot_file(save_txt(read_file_text(df[1, "path_abs"])), "read_file_text1.txt", binary = FALSE)
+  testthat::expect_snapshot_file(save_txt(read_file_text(df[2, "path_abs"])), "read_file_text2.txt", binary = FALSE)
+  testthat::expect_snapshot_file(save_txt(read_file_text(df[3, "path_abs"])), "read_file_text3.txt", binary = FALSE)
+  testthat::expect_snapshot_file(save_txt(read_file_text(df[4, "path_abs"])), "read_file_text4.txt", binary = FALSE)
+  testthat::expect_snapshot_file(save_txt(read_file_text(df[5, "path_abs"])), "read_file_text5.txt", binary = FALSE)
 })
 
 testthat::test_that("get_file_size returns the correct file size", {
